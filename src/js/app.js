10df4590a55da2eb6054c814480d20b23d35bf51
teejay74 <teejay744@gmail.com>
2020-12-03 22:07:58 +0500

    lint fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,14 @@
 (() => {
-    let currentHole = Math.floor((Math.random() * 16));
-    const hole = (i) => document.getElementById(`hole${i}`);
-    const hideHole = (i) => hole(i).className = 'hole';
-    const showHole = (i) => hole(i).className = 'hole hole_has-mole';
-    const nextHole = () => setTimeout(() => {
-      
-      hideHole(currentHole);
-      currentHole = Math.floor((Math.random() * 16));
-      showHole(currentHole);
-      nextHole();
-    }, 1000);
-  
+  let currentHole = Math.floor(Math.random() * 16);
+  const hole = (i) => document.getElementById(`hole${i}`);
+  const hideHole = (i) => hole(i).className = 'hole';
+  const showHole = (i) => hole(i).className = 'hole hole_has-mole';
+  const nextHole = () => setTimeout(() => {
+    hideHole(currentHole);
+    currentHole = Math.floor(Math.random() * 16);
+    showHole(currentHole);
     nextHole();
-  })();
+  }, 1000);
+
+  nextHole();
+})();
